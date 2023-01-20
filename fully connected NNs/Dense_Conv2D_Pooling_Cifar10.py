61bd083c424dc520dd8ec66a569528f0d3b928db
Pouya sohrabi nejad <95095794+PouyaSonej@users.noreply.github.com>
2023-01-20 21:03:16 +0330

    Create Dense_Conv2D_Pooling_Cifar10.py

---
+++ fully connected NNs/Dense_Conv2D_Pooling_Cifar10.py
@@ -0,0 +1,34 @@
+from tensorflow import keras
+import numpy as np
+from matplotlib import pyplot as plt
+
+# load dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
+# Convert class vectors to binary class matrices.
+num_classes = 10
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+"""# Pooling"""
+
+# define model with pooling
+model = keras.Sequential()
+model.add(keras.layers.Input(shape=x_train[0].shape))
+model.add(keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
+model.add(keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
+model.add(keras.layers.MaxPool2D())
+model.add(keras.layers.Flatten(input_shape=x_train[0].shape))
+model.add(keras.layers.Dense(units=num_classes, activation='softmax'))
+
+model.summary()
+
+# compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# train model
+history = model.fit(x_train, y_train,
+                    batch_size=100,
+                    epochs=10,
+                    validation_data=(x_test, y_test),
+                    shuffle=True)

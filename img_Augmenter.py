25862c048610e384c2778b9a8f0f92ab54622cf4
Pouya sohrabi nejad <95095794+PouyaSonej@users.noreply.github.com>
2023-01-24 21:01:24 +0330

    Create img_Augmenter.py

---
+++ img_Augmenter.py
@@ -0,0 +1,84 @@
+# Sometimes(0.5, ...) applies the given augmenter in 50% of all cases,
+# e.g. Sometimes(0.5, GaussianBlur(0.3)) would blur roughly every second
+# image.
+sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+
+# Define our sequence of augmentation steps that will be applied to every image.
+seq = iaa.Sequential(
+    [
+        #
+        # Apply the following augmenters to most images.
+        #
+        iaa.Fliplr(0.5), # horizontally flip 50% of all images
+
+        # crop some of the images by 0-10% of their height/width
+        sometimes(iaa.Crop(percent=(0, 0.1))),
+
+        # Apply affine transformations to some of the images
+        # - scale to 80-120% of image height/width (each axis independently)
+        # - translate by -20 to +20 relative to height/width (per axis)
+        # - rotate by -45 to +45 degrees
+        # - shear by -16 to +16 degrees
+        # - order: use nearest neighbour or bilinear interpolation (fast)
+        # - mode: use any available mode to fill newly created pixels
+        #         see API or scikit-image for which modes are available
+        # - cval: if the mode is constant, then use a random brightness
+        #         for the newly created pixels (e.g. sometimes black,
+        #         sometimes white)
+        iaa.Affine(
+            scale={"x": (0.9, 1.1), "y": (0.9, 1.1)},
+            translate_percent={"x": (-0.05, 0.05), "y": (-0.05, 0.05)},
+            rotate=(-15, 15),
+            shear=(-10, 10),
+            order=[0, 1],
+            cval=(0, 255),
+            mode=ia.ALL
+        ),
+
+        #
+        # Execute 0 to 5 of the following (less important) augmenters per
+        # image. Don't execute all of them, as that would often be way too
+        # strong.
+        #
+        iaa.SomeOf((0, 5),
+            [
+                # Blur each image with varying strength using
+                # gaussian blur (sigma between 0 and 3.0),
+                # average/uniform blur (kernel size between 2x2 and 7x7)
+                # median blur (kernel size between 3x3 and 11x11).
+                iaa.OneOf([
+                    iaa.GaussianBlur((0, 3.0)),
+                    iaa.AverageBlur(k=(2, 7)),
+                    iaa.MedianBlur(k=(3, 11)),
+                ]),
+
+                # Sharpen each image, overlay the result with the original
+                # image using an alpha between 0 (no sharpening) and 1
+                # (full sharpening effect).
+                iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),
+
+                # Add gaussian noise to some images.
+                # In 50% of these cases, the noise is randomly sampled per
+                # channel and pixel.
+                # In the other 50% of all cases it is sampled once per
+                # pixel (i.e. brightness change).
+                iaa.AdditiveGaussianNoise(
+                    loc=0, scale=(0.0, 0.05*255), per_channel=0.5
+                ),
+
+                # Add a value of -10 to 10 to each pixel.
+                iaa.Add((-10, 10), per_channel=0.5),
+
+                # Change brightness of images (50-150% of original value).
+                iaa.Multiply((0.5, 1.5), per_channel=0.5),
+
+                # Improve or worsen the contrast of images.
+                iaa.LinearContrast((0.5, 2.0), per_channel=0.5),
+            ],
+            # do all of the above augmentations in random order
+            random_order=True
+        )
+    ],
+    # do all of the above augmentations in random order
+    random_order=True
+)
